81832fa89d9e473bae4d50d33950f38d91be16dd
Hammer_Keith <keith@michaelrobertson.com>
2018-02-12 17:09:47 -0600

    merged index

--- server/index.js
+++ server/index.js
@@ -10,37 +10,21 @@ const express = require("express"),
   editReview = require("./controllers/editReview"),
   getReview = require("./controllers/getReview"),
   postReview = require("./controllers/postReview");
+  postThisReview = require("./controllers/postReview");
 require("dotenv").config();
 
-// Controller Functions
-const addUser = './controllers/addUser';
-const editUser = './controllers/editUser';
-const getUser = './controllers/getUser';
-const removeUser = './controllers/removeUser';
-const postReview = './controllers/postReview';
-const editReview = './controllers/editReview';
-const getReview = './controllers/getReview';
-const deleteReview = './controllers/deleteReview';
-
-// Database Connection
 massive(process.env.CONNECTION_STRING)
-  .then(dbInstance => app.set('db', dbInstance))
+  .then(dbInstance => app.set("db", dbInstance))
   .catch(console.log);
 
 // app.use(session(config.session));
 app.use(cors());
 app.use(json());
 app.use('/', express.static(__dirname));
+app.use(bodyParser.json());
+app.use("/", express.static(__dirname));
 
-// Server Endpoints
-app.get('/api/user', getUser);
-app.post('/api/user/add', addUser);
-app.put('/api/user/edit', editUser);
-app.delete('/api/user/remove', removeUser);
-app.get('/api/reviews', getReview);
-app.post('/api/reviews/add', postReview);
-app.put('/api/reviews/edit', editReview);
-app.delete('/api/reviews/remove', deleteReview);
+
 
 // yarn add express cors body-parser massive dotenv
 app.get('/api/deleteReview', deleteReview.deleteReview)
@@ -48,3 +32,7 @@ app.get('/api/editReview', editReview.editReview)
 app.get('/api/getReview', getReview.getReview)
 app.get('/api/postReview', postReview.postReview)
 app.listen(port, () => console.log('Server listening on port', port));
+app.get('/api/postReview', postThisReview.postReview)
+app.listen(port, function() {
+  console.log("Server listening on port", port);
+});
