860d51242342b2bcc5dfa9f7f832c9b091d0985c
ericdrewes <eric.d.drewes@gmail.com>
2018-02-13 16:07:51 -0600

    basic headers

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Register from "../authentication/Register";
 import Login from "../authentication/Login";
+import MainHeader from '../headers/mainHeader';
 // import picture from "./resources/signup_illustrationYelp.png";
 
 export default class Home extends Component {
@@ -9,13 +10,9 @@ export default class Home extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <button onClick={() => this.props.history.push("/login")}>Login</button>
-        <button onClick={() => this.props.history.push("/register")}>
-          Sign up
-        </button>
-      </div>
-    );
+  
+    return  (
+    <MainHeader />
+    )
   }
 }
