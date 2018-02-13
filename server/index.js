21d1f8bb61857bcbd7dcacd177eb7edee5453110
Brian Poole <30422326+bpoole5591@users.noreply.github.com>
2018-02-13 16:30:19 -0600

    Updated the server/index.js
    Updated the server/index.js to properly declare controller functions.

--- server/index.js
+++ server/index.js
@@ -6,10 +6,20 @@ const port = 3001;
 const app = express();
 const massive = require('massive');
 
+// Controller Function
+
+const { addUser } = require('./controllers/addUser');
+const { editUser } = require('./controllers/editUser');
+const { getUser } = require('./controllers/getUser');
+const { removeUser } = require('./controllers/removeUser');
+const { deleteReview } = require('./controllers/deleteReview');
+const { editReview } = require('./controllers/editReview');
+const { getReview } = require('./controllers/getReview');
+const { postReview } = require('./controllers/postReview');
 
 // Database Connection
 massive(process.env.CONNECTION_STRING)
-  .then(dbInstance => app.set('db', dbInstance))
+  .then(db => app.set('db', db))
   .catch(console.log);
 
 app.use(cors());
@@ -17,7 +27,7 @@ app.use(json());
 app.use('/', express.static(__dirname));
 
 // Server Endpoints
-app.get('/api/user', getUser);
+app.get('/api/user/:userid', getUser);
 app.post('/api/user/add', addUser);
 app.put('/api/user/edit', editUser);
 app.delete('/api/user/remove', removeUser);
